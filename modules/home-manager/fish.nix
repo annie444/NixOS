7dc6fd16380992daa1de41a121ddb60ed2f329da
Annie Ehler <annie.ehler.4@gmail.com>
2024-07-25 12:18:24 -0700

    Fzf colors should be declarative

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -131,11 +131,19 @@ in
           "--exact"
           "--preview 'bat -n 20 {}'"
         ];
-        colors = ''
-          --color=dark
-          --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
-          --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
-        '';
+        colors = {
+          fg = "-1";
+          bg = "-1";
+          hl = "#5fff87";
+          "fg+" = "-1";
+          "bg+" = "-1";
+          "hl+" = "#ffaf5f";
+          info = "#af87ff";
+          prompt = "#5fff87";
+          pointer = "#ff87d7";
+          marker = "#ff87d7";
+          spinner = "#ff87d7";
+        };
         tmux.enableShellIntegration = true;
         enableFishIntegration = true;
       };
