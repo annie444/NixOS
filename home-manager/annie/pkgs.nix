c2f97513403f53c2814db438e960a1a4a19c7456
Annie Ehler <annie.ehler.4@gmail.com>
2024-07-25 18:09:37 -0700

    Add c compilers

--- home-manager/annie/pkgs.nix
+++ home-manager/annie/pkgs.nix
@@ -36,5 +36,31 @@
     kubernetes-helm
     kustomize
     helmfile
+    valgrind
+
+    # build tools
+    cmake
+    gcc
+    gnumake
+    ninja
+    pkg-config
+
+    # generates .cache and compile_commands.json
+    # files required by clangd
+    bear
+
+    # provides clangd (LSP)
+    # provides libraries
+    # NOTE: make sure mason.nvim doesn't install clangd
+    clang-tools
+
+    # required by codelldb (debugger)
+    # lldb # libraries conflicts with clang-tools
+    gdb
+
+    # libs
+    gpp # c++ module?, decrypt
+    gecode # c++ module
+
   ];
 }
