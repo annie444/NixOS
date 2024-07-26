8e1a1f3e8d3ad5a55355d2b3a262ae2ec9aff1f6
Annie Ehler <annie.ehler.4@gmail.com>
2024-07-26 09:11:10 -0700

    Sops is available within config for users

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -75,24 +75,6 @@
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
 
-  sops.secrets."annie/password".neededForUsers = true;
-
-  programs.fish.enable = true;
-
-  users.users = {
-    annie = {
-      hashedPasswordFile = config.sops.secrets."annie/password".path;
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-	      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVVe3niALKXj/d7z0Bn27uF5e64GfjPgcWOXXwziJpW"
-				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvhfZJ3pXA5ZJ3+6PPI6NxvOg5E/y3kKZ1NxkfTKZoD"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMTijhGkE4C7yFWj0/md4R8EwLaYsyHKLTIOzJM65QHv"
-      ];
-      extraGroups = ["wheel"];
-      shell = pkgs.fish;
-    };
-  };
-  
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
---
+++ nixos/users.nix
@@ -0,0 +1,21 @@
+{ pkgs, config, ... }: {
+
+  sops.secrets."annie/password".neededForUsers = true;
+
+  programs.fish.enable = true;
+
+  users.users = {
+    annie = {
+      hashedPasswordFile = config.sops.secrets."annie/password".path;
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+	      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVVe3niALKXj/d7z0Bn27uF5e64GfjPgcWOXXwziJpW"
+				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvhfZJ3pXA5ZJ3+6PPI6NxvOg5E/y3kKZ1NxkfTKZoD"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMTijhGkE4C7yFWj0/md4R8EwLaYsyHKLTIOzJM65QHv"
+      ];
+      extraGroups = ["wheel"];
+      shell = pkgs.fish;
+    };
+  };
+
+}
